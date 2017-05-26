eb5872b0129c976d0d74a6480ffd2d8a804f8598
Your Name <eveleech@gmail.com>
2017-05-26 09:55:57 -0700

    introns detailed table

--- app/models/introns.rb
+++ app/models/introns.rb
@@ -5,8 +5,40 @@ class Introns < ApplicationRecord
         return introns
     end
 
+    def self.count_detailed_statistics(params)
+        gene_request = Genes.fix_true_false(params["gene_request"])
+        intron_request = Exons.fix_true_false(params["request"])
+        id_org_part = Genes.org_part(JSON.parse(params["org_ids"]))
+        request = "SELECT #{fields_for_select} FROM genes #{Genes.inner_join_for_org_name} #{inner_join_introns} WHERE #{id_org_part} AND #{gene_request} AND #{Genes.additional_gene_params} AND #{intron_request} AND #{additional_intron_params}"
+        return Exons.connection.execute(request)
+    end
+
+
+    def self.fix_true_false(request)
+        request = request.gsub("warning_n_in_sequence = 0","warning_n_in_sequence = false")
+        request = request.gsub("warning_n_in_sequence = 1","warning_n_in_sequence = true")
+        request = request.gsub("warning_n_in_sequence != 0","warning_n_in_sequence != false")
+        request = request.gsub("warning_n_in_sequence != 1","warning_n_in_sequence != true")
+        request = request.gsub("\"","")
+        return request        
+    end
+
+    def self.additional_intron_params
+        return "(introns.id_genes = genes.id) AND (introns.error_in_isoform = false) AND (introns.from_main_isoform = true)"
+    end
+
+    def self.inner_join_introns
+        return "INNER JOIN introns ON introns.id_genes = genes.id"
+    end
+
+    def self.fields_for_select
+        pairs = detailed_fields.zip(human_fields_names)
+        return pairs.map { |pair| "#{pair[0]} as #{pair[1]}" }.join(",")
+    end
+
     def self.detailed_fields
         return ["organisms.name","genes.name","genes.id_orthologous_groups","genes.ncbi_gene_id",
+            "introns.id_isoforms",
             "introns.id_intron_types","introns.start_dinucleotide","introns.end_dinucleotide","introns.startt",
             "introns.endd","introns.lengthh","introns.indexx","introns.length_phase",
             "introns.phase","introns.warning_start_dinucleotide",
@@ -15,6 +47,7 @@ class Introns < ApplicationRecord
 
     def self.human_fields_names
         return ["organism_name","gene_name","gene_orthologous_groups","gene_ncbi_gene",
+            "isoform_id",
             "intron_type","start_dinucleotide","end_dinucleotide","start",
             "end","length","indexx","length_phase",
             "phase","warning_start_dinucleotide",
